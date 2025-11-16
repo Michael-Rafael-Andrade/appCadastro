f801c1a49100d15c86bd188475b70f9c2c6a7d5f
Michael-Rafael-Andrade <michaelrafaeldeandrade@gmail.com>
2025-11-15 22:03:19 -0300

    Update produtos.js
    Implementando a Listagem de Produtos

--- crud-app/routes/produtos.js
+++ crud-app/routes/produtos.js
@@ -1,8 +1,21 @@
 // routes/produtos.js
-var express = require('express'); // Importa a biblioteca Express
-var router = express.Router(); // Inicializa e cria o objeto Router
 
+var express = require('express');
+var router = express.Router();
 
+// Importa o Model de Produtos
+var produtoModel = require('../models/ProdutoModel'); 
 
+// GET /produtos/listagem
+router.get('/listagem', function(req, res, next) {
+  
+  const listaDeProdutos = produtoModel.produtos; 
+  
+  // Renderiza a view views/produtos/listagem.hbs
+  res.render('produtos/listagem', { 
+    title: 'Listagem de Produtos', 
+    produtos: listaDeProdutos 
+  });
+});
 
 module.exports = router;
