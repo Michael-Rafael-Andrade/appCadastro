44b9460fea4764f2e9384f247b856b34d704b1ef
Michael-Rafael-Andrade <michaelrafaeldeandrade@gmail.com>
2025-11-15 22:01:35 -0300

    criando o index para testes
    criando o index para testes

---
+++ crud-app/routes/produtos.js
@@ -0,0 +1,8 @@
+// routes/produtos.js
+var express = require('express'); // Importa a biblioteca Express
+var router = express.Router(); // Inicializa e cria o objeto Router
+
+
+
+
+module.exports = router;
