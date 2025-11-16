307ad4b8c843e571907416fc54c964503b3f187e
Michael-Rafael-Andrade <michaelrafaeldeandrade@gmail.com>
2025-11-15 21:49:52 -0300

    Controller e Rota para Produtos
    Controller e Rota para Produtos

--- crud-app/app.js
+++ crud-app/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var pessoasRouter = require('./routes/pessoas');
+var produtosRouter = require('./routes/produtos');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/pessoas', pessoasRouter);
+app.use('/produtos', produtosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
